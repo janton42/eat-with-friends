88836634a1afe19bb8d4685410404b69129b0df6
janton42 <jantonstock@gmail.com>
2018-09-19 21:35:17 -0700

    removed cookie parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
@@ -35,7 +35,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
