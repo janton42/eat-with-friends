3908abe83de2d5fae5c715ab50e6ea0cff7d5ae6
janton42 <jantonstock@gmail.com>
2018-09-24 19:08:51 -0700

    started connecting okta for user authentication.

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
+var session = require('express-session');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var okta = require("@okta/okta-sdk-nodejs");
+var { ExpressOIDC } = require("@okta/oidc-middleware");
+
+var app = express();
+
+// user authentication with Okta
+var oktaClient = new okta.Client({
+  orgUrl: 'https://dev-243898.oktapreview.com',
+  token: '00rwcWvnXvMXiPO5iRqtcYF3skUMJdqtpMxad1z-WN'
+});
+const oidc = new ExpressOIDC({
+  issuer: "{yourOktaOrgUrl}/oauth2/default",
+  client_id: '0oagbjmic2bDzcX6V0h7',
+  client_secret: 'HVESJVVu-bNSW5iIyn7Tu82lV-HnvqNig4_Bvo6K',
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 // set up default mongoose connection
 mongoose.connect('mongodb://localhost/testdb', { useNewUrlParser: true }).then(() => {
@@ -21,10 +47,9 @@ var db = mongoose.connection;
 // bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const dashboardRouter = require('./routes/dashboard');
+const publicRouter = require('./routes/public');
+const usersRouter = require("./routes/users");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,16 +60,45 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+app.use('/', publicRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
 app.use('/users', usersRouter);
+// enable sessions
+app.use(session({
+	secret: '87ygjfeiefew3o358uhgbnjhghbgftydhbjkvslgt-oiubhuyegrh',
+	resave: true,
+	saveUninitialized: false
+}));
+// OIDC router for authentication
+app.use(oidc.router);
+// collect user data
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
 
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+};
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -56,5 +110,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
