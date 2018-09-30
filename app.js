934262bd72804f0e3b22876127c7d7e4a3d07ed0
janton42 <jantonstock@gmail.com>
2018-09-30 10:31:12 -0700

    added passport-session, and configured local strategy

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy (
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      return done(null, user);
+    });
+  }
+
+  ));
 
 // set up default mongoose connection
 mongoose.connect('mongodb://localhost/testdb', { useNewUrlParser: true }).then(() => {
